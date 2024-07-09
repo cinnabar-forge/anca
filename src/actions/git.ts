e26064ef631de4f2cfc6f8539533bf802dfaf8cb
Timur Moziev <code@timurrin.ru>
2024-07-09 21:46:31 +0000

    add gitignore check

--- src/actions/git.ts
+++ src/actions/git.ts
@@ -1,6 +1,60 @@
 import { AncaDevelopment } from "../schema.js";
 import { writeFolderFile } from "../utils.js";
 
+const IGNORE_ALL = `# Editor directories and files
+.idea
+.DS_Store
+*.suo
+*.ntvs*
+*.njsproj
+*.sln
+*.sw?
+.vscode
+
+# Logs
+logs
+*.log
+
+# Runtime data
+pids
+*.pid
+*.seed
+*.pid.lock
+`;
+
+const IGNORE_NODEJS = `# Built sources
+/dist
+/build
+*.spec
+
+# Dependency directories
+node_modules/
+jspm_packages/
+.yarn/cache
+.yarn/unplugged
+.yarn/build-state.yml
+.yarn/install-state.gz
+.pnp.*
+.yarn-integrity
+
+# Local settings
+.env
+.env.development.local
+.env.test.local
+.env.production.local
+.env.local
+
+# Node.js logs
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+pnpm-debug.log*
+lerna-debug.log*
+report.[0-9]*.[0-9]*.[0-9]*.[0-9]*.json
+
+# Output of 'npm pack'
+*.tgz`;
+
 const FILE_PATH = ".gitignore";
 
 /**
@@ -15,8 +69,12 @@ export async function checkGitIgnore(development: AncaDevelopment) {
   if (contents == null) {
     return false;
   }
-  contents;
-  return true;
+
+  if (development.state.config.stack === "nodejs") {
+    return contents === IGNORE_ALL + "\n" + IGNORE_NODEJS;
+  }
+
+  return contents === IGNORE_ALL;
 }
 
 /**
@@ -30,6 +88,8 @@ export async function fixGitIgnore(development: AncaDevelopment) {
   await writeFolderFile(
     development.fullPath,
     FILE_PATH,
-    development.state.files[FILE_PATH],
+    development.state.config.stack === "nodejs"
+      ? IGNORE_ALL + "\n" + IGNORE_NODEJS
+      : IGNORE_ALL,
   );
 }
