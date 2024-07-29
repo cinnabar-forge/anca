f0322673bc6a74756c2d8b7a2c48aaee2087da2b
Timur Moziev <code@timurrin.ru>
2024-07-29 05:24:10 +0000

    update nodejs versions comply anca

--- src/actions/variables.ts
+++ src/actions/variables.ts
@@ -1,5 +1,5 @@
 export const NODEJS_18_VERSION = "18.20.4";
-export const NODEJS_20_VERSION = "20.15.1";
-export const NODEJS_22_VERSION = "22.4.1";
+export const NODEJS_20_VERSION = "20.16.0";
+export const NODEJS_22_VERSION = "22.5.1";
 export const NODEJS_22_VERSION_SHA =
-  "addb41bd7d5bdef51dc3bd76292889692664b3c9de1b7c5f89aba95e474aee84"; // node-vX.Y.Z-linux-x64.tar.gz
+  "2a7b8b8aa5c739ae55233d59f78736911a8a5da5ea1c63f0e843da270d039499"; // https://nodejs.org/en/blog/release/vX.Y.Z node-vX.Y.Z-linux-x64.tar.gz
