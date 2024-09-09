5e7f7cdf27c18952b0c25e26ff5c061eef902a76
Timur Moziev <code@timurrin.ru>
2024-09-09 09:53:19 +0000

    add nodejs-openapi models generation

--- src/actions/nodejs-openapi.ts
+++ src/actions/nodejs-openapi.ts
@@ -7,13 +7,185 @@ import { capitalize, readFolderJsonFile } from "../utils.js";
 
 /**
  * Generate a fallback file name based on the method and api path
+ * @param operationId
  * @param method
  * @param apiPath
  */
-function generateFallbackFileName(method: string, apiPath: string): string {
+function generateOperationName(
+  operationId: string,
+  method: string,
+  apiPath: string,
+): string {
+  const sanitizedOperationId = sanitizeOperationId(operationId) || null;
+  if (sanitizedOperationId) {
+    return sanitizedOperationId;
+  }
   const hash = crypto.createHash("sha256");
   hash.update(`${apiPath}`);
   return method + capitalize(hash.digest("hex").substring(0, 8));
+}
+
+/**
+ * Sanitize the operationId to camelCase and remove non-alphanumeric characters
+ * @param operationId
+ * @returns sanitized operationId
+ */
+function sanitizeOperationId(operationId: string): string {
+  const parts = operationId.split(/[^a-zA-Z0-9]/).filter(Boolean);
+  const camelCaseId = parts
+    .map((part, index) => {
+      if (index === 0) return part.toLowerCase();
+      return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
+    })
+    .join("");
+
+  if (/^\d/.test(camelCaseId)) {
+    return (
+      "operation" + camelCaseId.charAt(0).toUpperCase() + camelCaseId.slice(1)
+    );
+  }
+
+  return camelCaseId;
+}
+
+/**
+ * Generate TypeScript models (interfaces) based on OpenAPI schema
+ * @param development
+ * @param openapi
+ */
+function generateTypeScriptModels(development: AncaDevelopment, openapi: any) {
+  const modelsDir = path.join(development.fullPath, "src", "models");
+  if (!fs.existsSync(modelsDir)) {
+    fs.mkdirSync(modelsDir);
+  }
+
+  const schemaComponents = openapi.components?.schemas || {};
+  const models: Record<string, string> = {};
+
+  const processSchema = function (schema: any, modelName: string) {
+    console.log(modelName, schema);
+    if (models[modelName]) {
+      console.log("Model already exists", modelName);
+      return;
+    }
+    const properties = Object.keys(schema.properties || {})
+      .map((propertyName) => {
+        const propertySchema = schema.properties[propertyName];
+        const propertyType = getPropertyType(propertySchema);
+        return `${propertyName}: ${propertyType};`;
+      })
+      .join("\n  ");
+
+    models[modelName] = `interface ${modelName} {
+  ${properties}
+}
+`;
+  };
+
+  let count = 0;
+
+  const processReferableSchema = function (schema: any, fallbackName?: string) {
+    if (!schema.$ref && schema.type === "object") {
+      processSchema(schema, fallbackName || `Generic${count++}`);
+    }
+  };
+
+  // preparing predefined schemas to be reused later
+  Object.keys(schemaComponents).forEach((modelName) => {
+    const schema = schemaComponents[modelName];
+    const interfaceName = `${capitalize(modelName)}`;
+    processSchema(schema, interfaceName);
+  });
+
+  Object.keys(openapi.paths).forEach((apiPath) => {
+    Object.keys(openapi.paths[apiPath]).forEach((method) => {
+      console.log(apiPath, method);
+      const operation = openapi.paths[apiPath][method];
+      const fileName = generateOperationName(
+        operation.operationId,
+        method,
+        apiPath,
+      );
+      const requestBody = operation.requestBody;
+      const parameters = operation.parameters;
+      const responses = operation.responses;
+
+      if (
+        requestBody &&
+        requestBody.content &&
+        requestBody.content["application/json"]
+      ) {
+        processReferableSchema(
+          requestBody.content["application/json"].schema,
+          capitalize(fileName + "JsonRequest"),
+        );
+      }
+
+      if (parameters) {
+        const schemaParams: Record<string, any> = {
+          type: "object",
+          properties: {},
+        };
+        const schemaQuery: Record<string, any> = {
+          type: "object",
+          properties: {},
+        };
+        parameters.forEach((parameter: any) => {
+          if (parameter.in === "path") {
+            schemaParams.properties[parameter.name] = parameter.schema;
+          } else if (parameter.in === "query") {
+            schemaQuery.properties[parameter.name] = parameter.schema;
+          }
+        });
+        processSchema(schemaParams, `${capitalize(fileName)}Params`);
+        processSchema(schemaQuery, `${capitalize(fileName)}Query`);
+      }
+
+      if (responses) {
+        Object.keys(responses).forEach((responseStatus) => {
+          const response = responses[responseStatus];
+          if (response.content && response.content["application/json"]) {
+            processReferableSchema(
+              response.content["application/json"].schema,
+              capitalize(fileName + responseStatus + "JsonResponse"),
+            );
+          }
+        });
+      }
+    });
+  });
+
+  Object.keys(models).forEach((modelName) => {
+    const modelContent = models[modelName];
+    const modelFile = path.join(modelsDir, `${modelName}.ts`);
+    fs.writeFileSync(modelFile, modelContent);
+  });
+}
+
+/**
+ *
+ * @param propertySchema
+ */
+function getPropertyType(propertySchema: any): string {
+  if (!propertySchema) {
+    return "any";
+  }
+  if (propertySchema.type === "array") {
+    return `Array<${getPropertyType(propertySchema.items)}> `;
+  } else if (propertySchema.type === "object") {
+    return getPropertyType(propertySchema.properties);
+  } else if (propertySchema.type === "string") {
+    return "string";
+  } else if (
+    propertySchema.type === "integer" ||
+    propertySchema.type === "number"
+  ) {
+    return "number";
+  } else if (propertySchema.type === "boolean") {
+    return "boolean";
+  } else {
+    return "any";
+  }
 }
 
 /**
@@ -29,6 +201,8 @@ export async function generateNodejsOpenapiFiles(development: AncaDevelopment) {
   if (!openapi) {
     return;
   }
+
+  generateTypeScriptModels(development, openapi);
 
   const controllersDir = path.join(development.fullPath, "src", "controllers");
   const servicesDir = path.join(development.fullPath, "src", "services");
@@ -60,8 +234,11 @@ export async function generateNodejsOpenapiFiles(development: AncaDevelopment) {
         .sort()
         .forEach((method) => {
           const operation = openapi.paths[apiPath][method];
-          const fileName =
-            operation.operationId || generateFallbackFileName(method, apiPath);
+          const fileName = generateOperationName(
+            operation.operationId,
+            method,
+            apiPath,
+          );
           const functionName = `route${capitalize(fileName)}`;
           pathsMethods.push({
             apiPath,
