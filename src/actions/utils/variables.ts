3ac4fd7ed3f8ea0e16221cb35d35938d4ae643e8
Timur Moziev <code@timurrin.ru>
2024-09-13 13:33:00 +0000

    update nodejs versions

--- src/actions/utils/variables.ts
+++ src/actions/utils/variables.ts
@@ -1,5 +1,5 @@
 export const NODEJS_18_VERSION = "18.20.4";
-export const NODEJS_20_VERSION = "20.16.0";
-export const NODEJS_22_VERSION = "22.6.0";
+export const NODEJS_20_VERSION = "20.17.0";
+export const NODEJS_22_VERSION = "22.8.0";
 export const NODEJS_22_VERSION_SHA =
-  "f2f4ccbcbc0a443e5fadebd1149a22f96087ec09cef52ff343a15ee835206d96"; // https://nodejs.org/en/blog/release/vX.Y.Z node-vX.Y.Z-linux-x64.tar.gz
+  "a0bc969f61b96575a4b2cdd239729aa3e721384fa9ca3fb926a9582c019c1bbc"; // https://nodejs.org/en/blog/release/vX.Y.Z node-vX.Y.Z-linux-x64.tar.gz
